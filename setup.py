6002f11d406bb2569c0ed50d570feae61346ac3f
DaniellaTahchiDS <daniellatahchi.com>
2025-07-10 19:28:24 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+edot='-e .'
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    this function returns the list of packages required
+    '''
+    r=[]
+    with open(file_path) as file_obj:
+        r=file_obj.readlines()
+        r=[req.replace("\n","") for req in r ]
+        if edot in r:
+            r.remove(edot)
+    return r
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Daniella',
+    author_email='daniellatahchi@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
